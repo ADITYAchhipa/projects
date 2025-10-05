7681a5c593d4bbd95c5235a6e91310cee7308469
ADITYA <adichhipa2@gmail.com>
2025-10-05 15:22:17 +0530

    sending model

---
+++ spline/src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/7NZDjzKuKo-8l1zq/scene.splinecode" />
+  );
+}
